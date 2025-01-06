cb3af2a45c6e14ecce2dc7234d30a0bd0ed47516
Dani2009Flo <danielflorin20@web.de>
2025-01-06 22:05:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 document.querySelectorAll('.animated-button').forEach((button) => {
     button.addEventListener('click', () => {
-        alert(`Du hast auf ${button.textContent} geklickt!`);
+        const url = button.getAttribute('data-url');
+        
+        if (url) {
+            // Wenn die URL vorhanden ist, weiterleiten
+            window.location.href = url;
+        } else {
+            // Für den "HELP"-Button (oder jeden Button ohne URL)
+            alert(`Du hast auf den ${button.textContent} Button geklickt!`);
+        }
     });
 });
