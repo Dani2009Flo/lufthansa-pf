0421cbec809d6ce2b7f215417857ffd802ab29b7
Dani2009Flo <danielflorin20@web.de>
2025-01-06 21:53:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-const express = require('express'); // Express Framework verwenden
-const path = require('path'); // Zum Umgang mit Dateipfaden
-
-const app = express();
-const PORT = 3000;
-
-// Statische Dateien (HTML und CSS) bereitstellen
 app.use(express.static(path.join(__dirname, '/')));
-
-// Server starten
-app.listen(PORT, () => {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
-});
